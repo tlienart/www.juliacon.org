9a2861618dbcff609cebc4c9a3cc6b3a78d6a607
tlienart <tlienart@me.com>
2021-01-20 09:56:05 +0000

    Deploying to gh-pages from  @ c1be2df67f2ffed645cadfd31ec2940a50d99daf 🚀

--- utils.jl
+++ utils.jl
@@ -17,3 +17,50 @@ function _box(md; title="", color="purple")
         </div>
         """)
 end
+
+function lx_avatar(lxc, _)
+    _, kwargs = lxargs(lxc)
+    return _avatar(; kwargs...)
+end
+
+function _avatar(; link="", img="", name="", affil="")
+    return html("""
+        <div class="u-vskip-3"></div>
+        <div class="header-talks avatar-talks">
+          <a href="$link">
+            <img class="avatar-circle-talks" src="$img">
+          </a>
+          <div class="speaker-talks">
+            <a href="$link" style="text-decoration:none;">$name</a>
+            <p class="affiliation">
+              $affil
+            </p>
+          </div>
+        </div>
+        <div class="u-vskip-2"></div>
+        """)
+end
+
+# NAV_YEAR tools
+
+curyear() = parse(Int, match(r"^\/?(20\d{2})\/", locvar(:fd_rpath)::String).captures[1])
+
+function hfun_curyear()
+    return "$(curyear())"
+end
+
+function hfun_previous_editions()
+    year = curyear()
+    pre_years = year-1:-1:2014
+    io = IOBuffer()
+    for (i, y) in enumerate(pre_years)
+        write(io, """<a href="/$y/">$y</a>""")
+        i == length(pre_years) || write(io, "/")
+    end
+    prevs = String(take!(io))
+    return """
+        <div class="u-futura u-uppercase previous-editions-menu" style="margin:auto">
+          Previously: $prevs
+        </div>
+        """
+end
